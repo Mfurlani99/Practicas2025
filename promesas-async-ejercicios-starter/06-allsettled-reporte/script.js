734ab6a224dfac30f3ab3b0faa1adf39e45d1297
Mfurlani99 <wolferd299@gmail.com>
2025-09-09 14:14:45 -0300

    6

--- promesas-async-ejercicios-starter/06-allsettled-reporte/script.js
+++ promesas-async-ejercicios-starter/06-allsettled-reporte/script.js
@@ -19,3 +19,36 @@ const tareaFail = async (ms, id) => { await delay(ms); throw new Error(`Fallo ${
 // Tip: results.filter(r => r.status === "fulfilled").map(r => r.value)
 
 console.log("Generá el reporte a partir de allSettled.");
+
+const p1 = tareaOk(500, 1);
+const p2 = tareaFail(200, 2);
+const p3 = tareaOk(800, 3);
+const p4 = tareaFail(100, 4);
+const p5 = tareaOk(400, 5);
+
+
+const promesas =[p1,p2,p3,p4,p5]
+
+
+const results = await Promise.allSettled(promesas);
+const resultadosOk = []
+const rejected = []
+
+for (let index = 0; index < results.length; index++) {
+    if (results[index].status === "fulfilled") {
+        resultadosOk.push(results[index].id)
+    }else{
+       rejected.push(results[index].status) 
+        rejected.push(results[index].reason.message) 
+    }    
+}
+
+Promise.allSettled(results).then((resultados) =>
+ resultados.forEach((resultados) => console.log(resultados.status)),
+console.log("el total es: " + results.length),
+console.log("el total de ok es: " + resultadosOk.length),
+console.log("el total de fallidos es: " + rejected.length),
+console.log(results),
+console.log(resultadosOk),
+console.log(rejected)
+);
